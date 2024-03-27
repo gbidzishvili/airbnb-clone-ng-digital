688e9a1653b452feb628795c4b86d02c36e5d9b9
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-27 18:08:01 +0400

    navigation added

---
+++ src/app/navigation/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}

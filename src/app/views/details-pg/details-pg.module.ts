f671b108d72f2e610a95dd1d00a03a9edaa60f7f
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-30 01:55:06 +0400

    details-pg added

--- src/app/views/details-pg/details-pg.module.ts
+++ src/app/views/details-pg/details-pg.module.ts
@@ -4,9 +4,10 @@ import { DetailsPgRoutingModule } from './details-pg-routing.module';
 import { MatListModule } from '@angular/material/list';
 import { DetailsPgComponent } from './details-pg.component';
 import { MatIconModule } from '@angular/material/icon';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
-    declarations: [DetailsPgComponent],
+    declarations: [DetailsPgComponent, CarouselComponent],
     imports: [
         CommonModule,
         DetailsPgRoutingModule,

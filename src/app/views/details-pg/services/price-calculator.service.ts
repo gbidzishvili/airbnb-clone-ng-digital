d158891aef4522d7425bd00b9e496bb96b4b00fd
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-03-31 03:58:45 +0400

    price-calculator service added

---
+++ src/app/views/details-pg/services/price-calculator.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PriceCalculatorService {
+    private totalSource = new BehaviorSubject<number>(0);
+    total$ = this.totalSource.asObservable();
+
+    private pricePerNight = 366; // Example price per night
+
+    updateNights(nights: number): void {
+        const total = nights * this.pricePerNight;
+        this.totalSource.next(total);
+    }
+}

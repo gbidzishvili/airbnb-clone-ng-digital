459c3842b3fa2c2c84d4608a391472323262eda2
giorgi <bidzishviligiorgi7@gmail.com>
2024-05-16 21:46:42 +0400

    filter bar added

---
+++ src/app/views/home-pg/containers/filters-container/filters-container.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filters-container',
+  templateUrl: './filters-container.component.html',
+  styleUrl: './filters-container.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FiltersContainerComponent {
+
+}

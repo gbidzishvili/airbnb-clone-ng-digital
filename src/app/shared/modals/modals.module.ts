d8bff2de48525846efa3fcf76ce7f04a297b5e35
giorgi <bidzishviligiorgi7@gmail.com>
2024-05-22 17:29:21 +0400

    some moduled added in the module

--- src/app/shared/modals/modals.module.ts
+++ src/app/shared/modals/modals.module.ts
@@ -7,9 +7,19 @@ import { MatListModule } from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MaterialModule } from '../modules/material/material.module';
 import { PriceRangeComponent } from './components-modal/price-range/price-range.component';
+import { MatSelectModule } from '@angular/material/select';
+import { ChooseCityComponent } from './components-modal/choose-city/choose-city.component';
+import { MinRatingComponent } from './components-modal/min-rating/min-rating.component';
+import { ChooseAmenitiesComponent } from './components-modal/choose-amenities/choose-amenities.component';
 
 @NgModule({
-    declarations: [FilterHotelsModalComponent, PriceRangeComponent],
+    declarations: [
+        FilterHotelsModalComponent,
+        PriceRangeComponent,
+        ChooseCityComponent,
+        MinRatingComponent,
+        ChooseAmenitiesComponent,
+    ],
     imports: [
         CommonModule,
         MatListModule,
@@ -17,6 +27,7 @@ import { PriceRangeComponent } from './components-modal/price-range/price-range.
         MaterialModule,
         ReactiveFormsModule,
         MatSliderModule,
+        MatSelectModule,
     ],
     exports: [FilterHotelsModalComponent],
 })

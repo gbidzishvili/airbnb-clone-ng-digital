24dfcd3b8ec8420666ab9e39be032a9d7fbb2d72
giorgi <bidzishviligiorgi7@gmail.com>
2024-05-22 17:33:26 +0400

    routing added for the second time

--- src/app/views/home-pg/home-pg-routing.module.ts
+++ src/app/views/home-pg/home-pg-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePgComponent } from './home-pg.component';
 import { authGuard } from '../../core/guard/auth.guard';
+import { logoutGuard } from '../../core/guard/logout.guard';
 
 const routes: Routes = [
-    { path: 'home', component: HomePgComponent, canActivate: [authGuard] },
+    { path: 'home', component: HomePgComponent, canActivate: [authGuard],canDeactivate: [logoutGuard] },
 ];
 
 @NgModule({

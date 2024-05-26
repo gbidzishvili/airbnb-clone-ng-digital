23daba1c88eb86af0ded8e3078dea4c4ca9a6d80
giorgi Bidzishvili <bidzishviligiorgi7@gmail.com>
2024-05-27 02:01:25 +0400

    last modificatoins

--- src/app/views/confirm-reservation-pg/confirm-reservation-pg.component.ts
+++ src/app/views/confirm-reservation-pg/confirm-reservation-pg.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { calculator } from '@igniteui/material-icons-extended';
+import { PriceCalculatorService } from '../details-pg/services/price-calculator.service';
 
 @Component({
-  selector: 'app-confirm-reservation-pg',
-  templateUrl: './confirm-reservation-pg.component.html',
-  styleUrl: './confirm-reservation-pg.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-confirm-reservation-pg',
+    templateUrl: './confirm-reservation-pg.component.html',
+    styleUrl: './confirm-reservation-pg.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ConfirmReservationPgComponent {
-
-}
+export class ConfirmReservationPgComponent {}

cdb23cd511809f78c624a4b2c352ddfd8b2df19b
giorgi <bidzishviligiorgi7@gmail.com>
2024-05-27 17:17:45 +0400

    hotel componenet added into shared module

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel/carousel.component';
 import { MaterialModule } from '../modules/material/material.module';
 import { ReservationSummaryComponent } from './reservation-summary/reservation-summary.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
-    declarations: [CarouselComponent, ReservationSummaryComponent],
-    imports: [CommonModule, MaterialModule],
-    exports: [CarouselComponent, ReservationSummaryComponent],
+    declarations: [
+        CarouselComponent,
+        ReservationSummaryComponent,
+        HotelComponent,
+    ],
+    imports: [CommonModule, MaterialModule, PipesModule],
+    exports: [CarouselComponent, ReservationSummaryComponent, HotelComponent],
 })
 export class SharedComponentsModule {}

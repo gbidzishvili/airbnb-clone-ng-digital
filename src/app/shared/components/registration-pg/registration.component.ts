68a9b874d9e156d901020bfec2c4117105845b1c
giorgi <bidzishviligiorgi7@gmail.com>
2024-05-27 19:28:22 +0400

    registration goes to the shared forlder

---
+++ src/app/shared/components/registration-pg/registration.component.ts
@@ -0,0 +1,69 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormValidationService } from '../../../views/authorization/services/form-validation.service';
+import { BaseProxyService } from '../../../core/services/base-proxy.service';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+
+@UntilDestroy()
+@Component({
+    selector: 'app-registration',
+    templateUrl: './registration.component.html',
+    styleUrl: './registration.component.scss',
+    // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegistrationComponent implements OnInit {
+    hidePass = true;
+    hideConfirm = true;
+    registerForm!: FormGroup;
+    options = ['male', 'female'];
+    constructor(
+        public validationSrv: FormValidationService,
+        public baseProxySrv: BaseProxyService,
+        public router: Router
+    ) {}
+    ngOnInit() {
+        this.router.events.pipe(untilDestroyed(this)).subscribe((url: any) => {
+            console.log(url.routerEvent.url);
+        });
+        this.registerForm = new FormGroup(
+            {
+                userName: new FormControl(null, Validators.required),
+                email: new FormControl(null, [
+                    Validators.required,
+                    Validators.email,
+                ]),
+                phoneNumber: new FormControl(null, [
+                    Validators.required,
+                    Validators.minLength(9),
+                    Validators.maxLength(9),
+                ]),
+                personalNumber: new FormControl(null, [
+                    Validators.required,
+                    Validators.maxLength(11),
+                    Validators.minLength(11),
+                ]),
+                gender: new FormControl(null, [Validators.required]),
+                password: new FormControl(null, [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(32),
+                    Validators.pattern(
+                        '^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*_.=+-]).+$'
+                    ),
+                ]),
+                confirmPassword: new FormControl(null, Validators.required),
+            },
+            this.validationSrv.passwordMatch()
+        );
+    }
+    onRegister() {
+        const { confirmPassword, ...newUser } = this.registerForm.value;
+        this.baseProxySrv
+            .create(
+                newUser,
+                'http://www.airbnb-digital-students.somee.com/api/user/registerUser'
+            )
+            .subscribe((v) => console.log('value is printed:', v));
+    }
+}

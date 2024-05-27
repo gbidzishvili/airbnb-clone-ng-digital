6bfa149b8aa82155f8afe2c72993adfa025b78c8
giorgi <bidzishviligiorgi7@gmail.com>
2024-05-27 19:27:58 +0400

    authorization added

--- src/app/views/authorization/authorization.module.ts
+++ src/app/views/authorization/authorization.module.ts
@@ -3,23 +3,18 @@ import { CommonModule } from '@angular/common';
 import { AuthorizationComponent } from './authorization.component';
 import { AuthorizationRoutingModule } from './authorization-routing.module';
 import { SignInComponent } from './pages/sign-in-pg/sign-in.component';
-import { RegistrationComponent } from './pages/registration-pg/registration.component';
+import { RegistrationComponent } from '../../shared/components/registration-pg/registration.component';
 import { MaterialModule } from '../../shared/modules/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-    declarations: [
-        AuthorizationComponent,
-        SignInComponent,
-        RegistrationComponent,
-    ],
+    declarations: [AuthorizationComponent, SignInComponent],
     imports: [
         CommonModule,
         AuthorizationRoutingModule,
         MaterialModule,
         ReactiveFormsModule,
-        MatSelectModule,
     ],
 })
 export class AuthorizationModule {}
